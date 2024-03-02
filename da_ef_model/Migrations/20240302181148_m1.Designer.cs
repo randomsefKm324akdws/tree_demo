dc6d807549bf7f7e647c09dcea99f836847ddff7
randomsefKm324akdws <146724574+randomsefKm324akdws@users.noreply.github.com>
2024-03-02 21:16:51 +0300

    migration 1

--- da_ef_model/Migrations/20240302181148_m1.Designer.cs
+++ da_ef_model/Migrations/20240302181148_m1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using da_ef_model;
 
@@ -11,9 +12,11 @@ using da_ef_model;
 namespace da_ef_model.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240302181148_m1")]
+    partial class m1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
