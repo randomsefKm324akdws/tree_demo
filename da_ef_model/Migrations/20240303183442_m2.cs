babc585783f5eae2a581f291660cab514d49512b
randomsefKm324akdws <146724574+randomsefKm324akdws@users.noreply.github.com>
2024-03-03 21:38:24 +0300

    removed inxex

---
+++ da_ef_model/Migrations/20240303183442_m2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace da_ef_model.Migrations
+{
+    /// <inheritdoc />
+    public partial class m2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @" EXEC ('
+                 drop index IX_Nodes_TreeName_ParentId on dbo.Nodes
+                ')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @" EXEC ('
+                    CREATE NONCLUSTERED INDEX [IX_Nodes_TreeName_ParentId] ON [dbo].[Nodes]
+                    (
+	                    [TreeName] ASC,
+	                    [ParentId] ASC
+                    )
+                ')");
+        }
+    }
+}
